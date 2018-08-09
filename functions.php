126f7008440f65373745589ab06ea0567191594e
Thaldos <vincent.oliver@wanadoo.fr>
2018-08-09 15:30:17 +0200

    Manage risk of pump and valve overheating

--- functions.php
+++ functions.php
@@ -112,7 +112,7 @@ function waterTheGardenNow() {
         sendNotification(
             'The garden has probably not been watered today because ' .
             'a error occurred during handling the relay module. It would be a good idea ' .
-            'to go to check the hardware system.\n' .
+            'to go to check the hardware system. ' .
             'Error : ' . $isOkOpen
         );
     }
@@ -128,7 +128,7 @@ function waterTheGardenNow() {
  * @return bool
  */
 function openThenCloseCarefullyThePump($delayOfWatering) {
-    $isOk = false;
+    $isOk = true;
 
     // Get number of sub round of watering needed to avoid overheating :
     $nbrOfWateringRound = floor($delayOfWatering / DELAY_MAX_CONSECUTIVE_RUNNING);
